3857ba4af8aa4d2e96b139fc7837e5d97df810df
divyanshityagi16 <divyanshi.2428cseai2@kiet.edu>
2026-05-18 11:52:06 +0530

    url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-backend-igya.onrender.com",
+  baseURL: "https://employee-backend-igya.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
